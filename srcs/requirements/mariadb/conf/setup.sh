50dedbe81c2593b989de56c02e799074e1f5969d
Draxuxe <louis.filloux53@gmail.com>
2022-11-23 13:49:16 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,10 +1,43 @@
-#!/bin/bash
+#!/bin/sh
 
-service mysql start
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
 
-mysql -u root -e "CREATE USER '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_USR_PWD}';"
-mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root wordpress  < /wordpress.sql
-mysql -u root -e "USE wordpress; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USR}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	
+	chown -R mysql:mysql /var/lib/mysql
 
-mysql -u root -e "alter user 'root'@'localhost' identified by 'password'";
+	# init database
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+		return 1
+	fi
+
+	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+	cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM	mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
+CREATE DATABASE $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USR'@'%' IDENTIFIED by '$MYSQL_USR_PWD';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MYSQL_USR'@'%';
+FLUSH PRIVILEGES;
+EOF
+	# run init.sql
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+	rm -f $tfile
+fi
+
+# allow remote connections
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console
