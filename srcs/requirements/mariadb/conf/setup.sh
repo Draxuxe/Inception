94569215b5ea3fd9f2b6b19c9d0a77991dad8cbf
Draxuxe <louis.filloux53@gmail.com>
2022-11-23 13:19:08 +0100

    Makefile

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -2,7 +2,7 @@
 
 service mysql start
 
-mysql -u root -e "CREATE USER '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_PWD}';"
+mysql -u root -e "CREATE USER '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_USR_PWD}';"
 mysql -u root -e "CREATE DATABASE wordpress;"
 mysql -u root wordpress  < /wordpress.sql
 mysql -u root -e "USE wordpress; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USR}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
