033534763ef97528194daa8d483427e2442e37ff
Draxuxe <louis.filloux53@gmail.com>
2022-11-23 15:05:28 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -32,7 +32,7 @@ GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$MYSQL_USR'@'%';
 FLUSH PRIVILEGES;
 EOF
 	# run init.sql
-	/var/lib/mysqld --user=mysql --bootstrap < $tfile
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
 	rm -f $tfile
 fi
 
@@ -40,4 +40,4 @@ fi
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec /var/lib/mysqld --user=mysql --console
+exec /usr/bin/mysqld --user=mysql --console
