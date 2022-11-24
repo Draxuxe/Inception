268d2673ecc56a9248403ea267029300e7b42c81
Draxuxe <louis.filloux53@gmail.com>
2022-11-24 10:27:08 +0100

    Wordpress

--- srcs/requirements/wordpress/conf/wp_start.sh
+++ srcs/requirements/wordpress/conf/wp_start.sh
@@ -1,30 +1,20 @@
 #!bin/bash
-sleep 5
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
-# wp-config.php file
-    wp config create	--allow-root \
-    					--dbname=$WP_DB_NAME \
-    					--dbuser=$MYSQL_USR\
-    					--dbpass=$MYSQL_USR_PWD \
-    					--dbhost=mariadb:3306 --path='/var/www/wordpress'
-    echo "asdf"
-    sleep 2
-    # --allow-root
-    wp core install     --allow-root --url=$DOMAIN_NAME \
-    					--title="My first ever Inception!" \
-    					--admin_user=$MYSQL_ROOT_USR \
-                        --admin_password=$MYSQL_ROOT_PWD \
-                        --admin_email=$ROOT_EMAIL \
-                        --path='/var/www/wordpress'
-    wp user create      --allow-root $MYSQL_USR $USR_EMAIL \
-    					--user_pass=$MYSQL_USR_PWD \
-    					--role=author\
-                        --path='/var/www/wordpress' >> /log.txt
-fi
+cd /var/www/wordpress
+wp core config	--dbhost=$DB_HOST \
+				--dbname=$DB_NAME \
+				--dbuser=$DB_USER \
+				--dbpass=$DB_PASSWORD \
+				--allow-root
 
-# foreground
-mkdir ./run/php/
-/usr/sbin/php-fpm7.3 -F
+wp core install --title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_MAIL \
+				--url=$WP_URL \
+				--allow-root
 
-# user 
-# wp user list --allow-root
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+cd -
+
+# run php-fpm7.3 listening for CGI request
+php-fpm7.3 -F
