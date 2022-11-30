03c3d86e741842cb80ca0e768afd7a5fb0386f73
Draxuxe <louis.filloux53@gmail.com>
2022-11-30 14:01:49 +0100

    Wordpress

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-service mysql start;
+CREATE DATABASE Base;
+CREATE USER 'lfilloux'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON Base.* TO 'lfilloux'@'%';
+FLUSH PRIVILEGES;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_HOST;"
-mysql -e "CREATE USER '$MYSQL_USR'@'%' IDENTIFIED BY '$MYSQL_USR_PWD';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USR'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';"
-
-exec mysqld_safe
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
