60a2bd27b5de923547a8b647542778c619dfec52
Draxuxe <louis.filloux53@gmail.com>
2022-12-02 16:04:18 +0100

    Wordpress

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -5,4 +5,5 @@ CREATE USER lfilloux@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON wordpress.* TO lfilloux@'%';
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root');
 EOF
