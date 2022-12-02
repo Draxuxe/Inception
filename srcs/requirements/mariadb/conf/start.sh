f4b8cc5b909aa378219cdab0f1ecb87f6174869c
Draxuxe <louis.filloux53@gmail.com>
2022-12-02 13:44:19 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 cat > setup.sql << EOF
 CREATE DATABASE IF NOT EXISTS mariadb;
-CREATE USER lfilloux@'%' IDENTIFIED BY pass;
+CREATE USER lfilloux@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON mariadb.* TO lfilloux@'%';
 FLUSH PRIVILEGES;
-ALTER USER louis@'localhost' IDENTIFIED BY root;
+ALTER USER louis@'localhost' IDENTIFIED BY 'root';
 EOF
