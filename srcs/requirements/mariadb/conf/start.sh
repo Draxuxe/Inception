580589564f114a97369354e1933d7f664d3fd1bb
Draxuxe <louis.filloux53@gmail.com>
2022-12-02 13:28:29 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
-service mysql start;
+# !/bin/bash
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
-mysql -e "CREATE USER '$MYSQL_USR'@'%' IDENTIFIED BY '$MYSQL_USR_PWD';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USR'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';"
-
-exec mysqld_safe
+cat > setup.sql << EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};
+CREATE USER '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_USR_PWD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USR}'@'%';
+FLUSH PRIVILEGES;
+ALTER USER '${MYSQL_ROOT_USR}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';
+EOF
