182c3e26aa4b5bab26ab969749a6d53d09cba948
Draxuxe <louis.filloux53@gmail.com>
2022-12-02 11:52:15 +0100

    Mariadb

---
+++ srcs/requirements/mariadb/conf/db_setup.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+export $(cat /home/lfilloux/DOcuments/Inception/srcs/.env | egrep -v "(^#.*|^$)" | xargs)
+
+sed -i "s/{MYSQL_ROOT_USR}/${MYSQL_ROOT_USR}/g" start.sh
+sed -i "s/{MYSQL_ROOT_PWD}/${MYSQL_ROOT_PWD}/g" start.sh
+sed -i "s/{MYSQL_DB}/${MYSQL_DB}/g" start.sh
+sed -i "s/{MYSQL_USR}/${MYSQL_USR}/g" start.sh
+sed -i "s/{MYSQL_USR_PWD}/${MYSQL_USR_PWD}/g" start.sh
