28293527a468be2e4c7c6323aeab37d982bf2ee5
Draxuxe <louis.filloux53@gmail.com>
2022-12-05 11:23:12 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,25 +1,10 @@
-#!/bin/bash
+# !/bin/bash
+cat > setup.sql << EOF
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER lfilloux@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON wordpress.* TO lfilloux@'%';
+FLUSH PRIVILEGES;
 
-if [ ! -d /var/lib/mysql/wordpress ]; then
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';
 
-    mysql_install_db --datadir /var/lib/mysql
-    service mysql start
-    sleep 3
-
-    mysql -e "CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-    mysql -e "CREATE USER IF NOT EXISTS lfilloux@'%' IDENTIFIED BY 'pass';"
-    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'lfilloux'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
-    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'lfilloux'@'localhost' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
-    mysql -e "FLUSH PRIVILEGES;"
-
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';"
-
-    sed -i "s/password =/password = god #/" /etc/mysql/debian.cnf
-
-    service mysql stop
-    echo "Database setup"
-else
-    echo "Database already setup"
-fi
-
-exec "$@"
+EOF
