a8ce0a923c95591c4e9f1671064bcd4457fdd548
Draxuxe <louis.filloux53@gmail.com>
2022-12-05 15:19:53 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -4,8 +4,8 @@ cat > setup.sql << EOF
 CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE USER lfilloux@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON wordpress.* TO lfilloux@'%';
-CREATE USER root@'localhost' IDENTIFIED BY 'god';
-GRANT ALL PRIVILEGES ON wordpress.* TO root@'localhost';
 FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';
 
 EOF
