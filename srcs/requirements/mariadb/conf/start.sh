065ead516d85f3aec3d3bbd8622a46df723ec32a
Draxuxe <louis.filloux53@gmail.com>
2022-12-05 15:38:25 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,11 +1,25 @@
-# !/bin/bash
-cat > setup.sql << EOF
+#!/bin/bash
 
-CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER lfilloux@'%' IDENTIFIED BY 'pass';
-GRANT ALL PRIVILEGES ON wordpress.* TO lfilloux@'%';
-FLUSH PRIVILEGES;
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';
+    mysql_install_db --datadir /var/lib/mysql
+    service mysql start
+    sleep 3
 
-EOF
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+    mysql -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;"
+    mysql -e "FLUSH PRIVILEGES;"
+
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+    sed -i "s/password =/password = ${MYSQL_ROOT_PASSWORD} #/" /etc/mysql/debian.cnf
+
+    service mysql stop
+    echo "Database setup"
+else
+    echo "Database already setup"
+fi
+
+exec "$@"
