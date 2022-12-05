dc2cd93d5c477fac051e5b301cc01cf5a8a134e8
Draxuxe <louis.filloux53@gmail.com>
2022-12-05 15:50:31 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! -d /var/lib/mysql/mariadb ]; then
+if [ ! -d /var/lib/mysql/wordpress_db ]; then
 
     mysql_install_db --datadir /var/lib/mysql
     service mysql start
     sleep 3
 
-    mysql -e "CREATE DATABASE IF NOT EXISTS mariadb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+    mysql -e "CREATE DATABASE IF NOT EXISTS wordpress_db DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
     mysql -e "CREATE USER IF NOT EXISTS lfilloux@'%' IDENTIFIED BY 'pass';"
-    mysql -e "GRANT ALL PRIVILEGES ON mariadb.* TO 'lfilloux'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
-    mysql -e "GRANT ALL PRIVILEGES ON mariadb.* TO 'lfilloux'@'localhost' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
+    mysql -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'lfilloux'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
+    mysql -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'lfilloux'@'localhost' IDENTIFIED BY 'pass' WITH GRANT OPTION;"
     mysql -e "FLUSH PRIVILEGES;"
 
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';"
