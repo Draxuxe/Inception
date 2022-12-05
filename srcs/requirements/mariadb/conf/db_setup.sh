ecd400b033d2ce6467ec42b1a9be84dc3c4010da
Draxuxe <louis.filloux53@gmail.com>
2022-12-05 10:27:11 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/db_setup.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-sed -i "s/{MYSQL_ROOT_USR}/${MYSQL_ROOT_USR}/g" start.sh
-sed -i "s/{MYSQL_ROOT_PWD}/${MYSQL_ROOT_PWD}/g" start.sh
-sed -i "s/{MYSQL_DB}/${MYSQL_DB}/g" start.sh
-sed -i "s/{MYSQL_USR}/${MYSQL_USR}/g" start.sh
-sed -i "s/{MYSQL_USR_PWD}/${MYSQL_USR_PWD}/g" start.sh
