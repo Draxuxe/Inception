7eb9e71c1245699005d032313f82af2420b7429d
Draxuxe <louis.filloux53@gmail.com>
2022-12-05 14:08:41 +0100

    Mariadb

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -3,8 +3,8 @@ cat > setup.sql << EOF
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER lfilloux@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON wordpress.* TO lfilloux@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';
 FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'god';
 
 EOF
