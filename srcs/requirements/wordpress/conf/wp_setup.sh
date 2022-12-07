5ca969234edf210b37088b84157e4f5b4ec0641e
Draxuxe <louis.filloux53@gmail.com>
2022-12-07 09:58:42 +0100

    Nginx

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-while ! mariadb --host=mariadb --user=louis --password=pass wordpress &> /dev/null; do
-        echo "Waiting on mariadb config"
-        sleep 2
-    done
-
 if [ ! -f wp-config.php ]; then
     echo "Installing wordpress"
     wp config create --allow-root --dbname=wordpress --dbuser=louis --dbpass=pass --dbhost=mariadb:3306 --prompt=pass --quiet
