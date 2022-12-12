324813346b08fc1ce52a734c6a8f31a3f2c20aa0
Draxuxe <louis.filloux53@gmail.com>
2022-12-12 14:08:50 +0100

    Nginx

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -1,12 +1,11 @@
-mkdir /var/www
 wget https://wordpress.org/latest.tar.gz
 tar xvf latest.tar.gz
 rm -rf latetest.tar.gz
-mv wordpress/ /var/www/app
+mv wordpress/ /var/www/html
 
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
-cd /var/www/app
+cd /var/www/html
 sed -i "s/username_here/lfilloux/g" wp-config-sample.php
 sed -i "s/password_here/pass/g" wp-config-sample.php
 sed -i "s/localhost/mariadb/g" wp-config-sample.php
