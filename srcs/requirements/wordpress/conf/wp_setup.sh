e4abd87e8fb68e6db8419c5a7af640f8e18870cf
Draxuxe <louis.filloux53@gmail.com>
2022-12-12 13:38:37 +0100

    Nginx

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -2,7 +2,7 @@ mkdir /var/www
 wget https://wordpress.org/latest.tar.gz
 tar xvf latest.tar.gz
 rm -rf latetest.tar.gz
-mv wordpress/ /var/www/app
+mv wordpress/ /var/www/html
 
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
