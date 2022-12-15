7274112c1a3e96190941bc08a2b5485f19c1326a
Draxuxe <louis.filloux53@gmail.com>
2022-12-15 11:03:42 +0100

    Env Mariadb Wordpress

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -7,6 +7,7 @@ mv wordpress/ /var/www/html
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
 cd /var/www/html
+
 sed -i "s/username_here/${MYSQL_USR}/g" wp-config-sample.php
 sed -i "s/password_here/${MYSQL_USR_PWD}/g" wp-config-sample.php
 sed -i "s/localhost/${MYSQL_HOST}/g" wp-config-sample.php
