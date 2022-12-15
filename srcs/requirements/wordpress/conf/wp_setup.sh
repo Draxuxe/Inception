dd163f2200b4a85d818677a6b1549541a3d2c3e7
Draxuxe <louis.filloux53@gmail.com>
2022-12-15 16:18:08 +0100

    Env Mariadb Wordpress

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -2,16 +2,17 @@ mkdir /var/www
 wget https://wordpress.org/latest.tar.gz
 tar xvf latest.tar.gz
 rm -rf latetest.tar.gz
-mv wordpress/ /var/www/app
+rm -rf var/www/html/index.nginx-debian.html
+mv wordpress/ /var/www/html
 
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
-cd /var/www/app
+cd /var/www/html/wordpress
 
 sed -i "s/username_here/${MYSQL_USR}/g" wp-config-sample.php
 sed -i "s/password_here/${MYSQL_USR_PWD}/g" wp-config-sample.php
-sed -i "s/localhost/mariadb/g" wp-config-sample.php
-sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
+sed -i "s/localhost/${MYSQL_HOST}/g" wp-config-sample.php
+sed -i "s/database_name_here/${MYSQL_DB}/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
 
 service php7.3-fpm start
