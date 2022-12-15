69a5e5a1a1f90ddcc5f143b87bc4ee54b5112934
Draxuxe <louis.filloux53@gmail.com>
2022-12-15 11:42:58 +0100

    Env Mariadb Wordpress

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -2,11 +2,11 @@ mkdir /var/www
 wget https://wordpress.org/latest.tar.gz
 tar xvf latest.tar.gz
 rm -rf latetest.tar.gz
-mv wordpress/ /var/www/app
+mv wordpress/ /var/www/html
 
 mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
 
-cd /var/www/app
+cd /var/www/html
 
 sed -i "s/username_here/${MYSQL_USR}/g" wp-config-sample.php
 sed -i "s/password_here/${MYSQL_USR_PWD}/g" wp-config-sample.php
