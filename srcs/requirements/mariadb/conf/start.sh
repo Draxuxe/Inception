a68aad4925d927778ddd7adecbe76fc91cac8b6e
Draxuxe <louis.filloux53@gmail.com>
2022-12-16 15:35:33 +0100

    je prie

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,10 +1,16 @@
 # !/bin/bash
-cat > setup.sql << EOF
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER ${MYSQL_USR}@'%' IDENTIFIED BY '${MYSQL_USR_PWD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO ${MYSQL_USR}@'%';
-FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';
+service mysql start
 
-EOF
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+mysql -e "CREATE USER IF NOT EXISTS ${MYSQL_USR}@'%' IDENTIFIED BY '${MYSQL_USR_PWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO '${MYSQL_USR}'@'%' IDENTIFIED BY '${MYSQL_USR_PWD}' WITH GRANT OPTION;"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "ALTER USER '${MYSQL_ROOT_USR}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';"
+
+sed -i "s/password =/password = ${MYSQL_ROOT_PWD} #/" /etc/mysql/debian.cnf
+
+service mysql stop
+
+exec "$@"
