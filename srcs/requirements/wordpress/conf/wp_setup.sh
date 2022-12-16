9fa239fa87c2122e1cd7baeaafa1471f50ced243
Draxuxe <louis.filloux53@gmail.com>
2022-12-16 12:31:25 +0100

    je prie

--- srcs/requirements/wordpress/conf/wp_setup.sh
+++ srcs/requirements/wordpress/conf/wp_setup.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
-if [ ! -f wp-config.php ]; then
-    wp config create --allow-root --dbname=${MYSQL_DB} --dbuser=${MYSQL_USR} --dbpass=${MYSQL_USR_PWD} --dbhost=${MYSQL_HOST}:3306 --prompt=${MYSQL_USR_PWD} --quiet
-    wp core install --allow-root --url='lfilloux.42.fr' --title='INCEPTION' --admin_user=${MYSQL_ROOT_USR} --admin_password=${MYSQL_ROOT_PWD} --admin_email=lfilloux@student.42lyon.fr --skip-email
-    wp user create --allow-root ${MYSQL_USR} "${MYSQL_USR}"@randomuser.com --role='subscriber' --user_pass=${MYSQL_USR}
-    wp theme install twentytwenty --activate --allow-root
-fi
+# !/bin/bash
+
+mkdir /var/www
+wget https://wordpress.org/latest.tar.gz
+tar xvf latest.tar.gz
+rm -rf latetest.tar.gz
+mv wordpress/ /var/www/app
+
+mv fpm.cnf /etc/php/7.3/fpm/pool.d/www.conf
+
+cd /var/www/app
+
+sed -i "s/username_here/${MYSQL_USR}/g" wp-config-sample.php
+sed -i "s/password_here/${MYSQL_USR_PWD}/g" wp-config-sample.php
+sed -i "s/localhost/${MYSQL_HOST}/g" wp-config-sample.php
+sed -i "s/database_name_here/${MYSQL_DB}/g" wp-config-sample.php
+mv wp-config-sample.php wp-config.php
+
+service php7.3-fpm start
 
 exec "$@"
