cdcf631dad1eb2a21dc80762c81a7d553d215115
Draxuxe <louis.filloux53@gmail.com>
2022-12-16 15:25:41 +0100

    je prie

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -3,7 +3,6 @@ cat > setup.sql << EOF
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER louis@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO louis@'%';
-GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO louis@'localhost';
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PWD}';
